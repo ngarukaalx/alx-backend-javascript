6e309177c4879aed549917310960d27ec9cf4856
ngarukaalx <hiramkabuagi@gmail.com>
2024-03-06 11:02:58 +0000

    promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,6 +1,7 @@
-//Returns a promise
-export default function getResponseFromAPI(){
-    return new Promise((resolve, reject) => {
-        //perform asynchronous operations
-    });
+// Returns a promise
+export default function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    resolve();
+    reject();
+  });
 }
