ab2da0c4d7794ab719c72f1a8ce5b020c3d3901d
ngarukaalx <hiramkabuagi@gmail.com>
2024-03-09 08:56:13 +0000

    pricing

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,43 @@
+// import class currency
+import Currency from './3-currency';
+
+// export class
+export default class pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  // getter for amount
+  get amount() {
+    return this._amount;
+  }
+
+  // getttr for currency
+  get currency() {
+    return this._currency;
+  }
+
+  // setter for amount
+  set amount(newAmount) {
+    this._amount = newAmount;
+  }
+
+  // setter for currency
+  set currency(newCurrency) {
+    this._currency = newCurrency;
+  }
+
+  // returns attribute in the format, amount currency_name (currency_code)
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  // Return amount multipied by conversion
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount === 'number' && typeof conversionRate === 'number') {
+      throw new Error('amount or conversionrate must be numbers');
+    }
+    return amount * conversionRate;
+  }
+}
