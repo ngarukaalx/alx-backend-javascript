0c288419854874a4c7115e547d4f638ddb507c46
ngarukaalx <hiramkabuagi@gmail.com>
2024-04-30 21:26:46 +0000

    checknet

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,6 +1,51 @@
-// Create a small HTTP server using Node's HTTP module
+// Crea;;te a small HTTP server using Node's HTTP module
 const { createServer } = require('http');
-const countStudents = require('./3-read_file_async');
+// function to read file and perform some required operations
+// Reading a file asynchronously
+function countStudents (path) {
+        const fs = require('fs');
+        // return a promise
+        return new Promise((resolve, reject) => {
+                // Read the file asynchronously
+                fs.readFile(path, 'utf8', (err, data) => {
+                        if (err) {
+                                reject(new Error('Cannot load the database'));
+                        } else {
+                                // declare variables to store counts and list students
+                                let numberStudents = 0;
+                                let swe = 0;
+                                let cs = 0;
+                                let listCs = [];
+                                let listSw = [];
+                                const lines = data.split('\n');
+                                for (let i = 1; i < lines.length; i++) {
+                                        // skip empty lines
+                                        if (lines[i].trim() === '') {
+                                                continue;
+                                        }
+                                        const values = lines[i].trim().split(',');
+                                        numberStudents++;
+                                        if (values[3] === 'SWE') {
+                                                listSw.push(values[0]);
+                                                swe++;
+                                        }
+                                        if (values[3] === 'CS') {
+                                                listCs.push(values[0]);
+                                                cs++;
+                                        }
+                               }
+                               let result = `Number of students: ${numberStudents}\n`;
+                                if (cs) {
+                                        result += `Number of students in CS: ${cs}. List: ${listCs}\n`;
+                                }
+                                if (swe) {
+                                        result += `Number of students in CS: ${swe}. List: ${listSw}`;
+                                }
+                                resolve(result);
+                        }
+                })
+        });
+}
 
 // declare a variable to store the intro part
 let intro = 'This is the list of our students\n';
