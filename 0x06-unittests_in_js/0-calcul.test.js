bc750eeaa69d78d4b3ae632583ce5731940c8b69
ngarukaalx <hiramkabuagi@gmail.com>
2024-05-03 18:29:56 +0000

    checks burdg

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -3,16 +3,24 @@ const assert = require('assert');
 const  calculateNumber = require('./0-calcul.js');
 
 describe('calculateNumber', () => {
-  it('returns expected 4', () => {
-    assert.equal(calculateNumber(10, 3), 13);
-  });
-  it('returns expected 5', () => {
-    assert.equal(calculateNumber(1, 3.7), 5);
-  });
-  it('returns expected', () => {
-    assert.equal(calculateNumber(3.7, 1), 5);
-  });
-  it('returns expected 6', () => {
-    assert.equal(calculateNumber(1.5, 3.7), 6);
-  });
+    describe('sum with both not rounded', () => {
+        it('returns expected 4', () => {
+            assert.equal(calculateNumber(10, 3), 13);
+	});
+    });
+    describe('sum with second rounded', () => {
+        it('returns expected 5', () => {
+            assert.equal(calculateNumber(1, 3.7), 5);
+	});
+    });
+    describe('sum with first rounded', () => {
+        it('returns expected', () => {
+            assert.equal(calculateNumber(3.7, 1), 5);
+	});
+    });
+    describe('sum with both rounded', () => {
+        it('returns expected 6', () => {
+            assert.equal(calculateNumber(1.5, 3.7), 6);
+	});
+    });
 });
