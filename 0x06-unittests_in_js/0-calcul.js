bf67ded9f0e07d00e25d1faf69397b1ee7611a19
ngarukaalx <hiramkabuagi@gmail.com>
2024-05-03 16:42:15 +0000

    Basic test with Mocha and Node assertion library

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,10 +1,8 @@
 // accepts two arguments (number) a and b
-module.exports = {
-  // accepts two arguments (number) a and b
-  // round a and b and return the sum of it
-  calculateNumber(a, b) {
-    const roundA = Math.round(a);
-    const roundB = Math.round(b);
-    return roundA + roundB;
-  },
+// round a and b and return the sum of it
+const calculateNumber = (a, b) => {
+  const roundA = Math.round(a);
+  const roundB = Math.round(b);
+  return roundA + roundB;
 };
+module.exports = calculateNumber;
--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,6 +1,6 @@
 // import the calculateNumber
 const assert = require('assert');
-const { calculateNumber } = require('./0-calcul.js');
+const  calculateNumber = require('./0-calcul.js');
 
 describe('calculateNumber', () => {
   it('returns expected 4', () => {
